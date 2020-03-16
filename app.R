be94bc44ef2a32e7ccc59c937b69fa1e8d214f5a
Luis Miguel Montilla <luismmontilla@usb.ve>
2020-03-16 05:03:54 +0100

    added national trend

--- app.R
+++ app.R
@@ -2,35 +2,47 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 
+#data
+url_a <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
 
-urlfile<-"https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+region <- read.csv(url_a)
+region$data <- as.POSIXct(sub(" .*", "", region$data))
 
-df <- read.csv(urlfile)
-df$data <- as.POSIXct(df$data)
+url_b <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+nation <- read.csv(url_b)
+nation$data <- as.POSIXct(sub(" .*", "", nation$data))
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("COVID-19: Number of new daily cases in Italy"),
+    titlePanel("COVID-19: Number of new cases in Italy"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             selectInput("regionInput", "Region",
-                        choices = levels(df$denominazione_regione))
+                        choices = levels(region$denominazione_regione)),
+            checkboxInput("nationInput", 
+                          label = "Show national trend", 
+                          value = FALSE),
+            p("All the data displayed in this visualization is provided by the 
+             Italian Ministry of Health (Ministero della Salute) and elaborated 
+             by Dipartimento della Protezione Civile. This work is therefore a 
+             derivative of ",
+              a("COVID-19 Italia - Monitoraggio situazione",
+                href = "https://github.com/pcm-dpc/COVID-19"),
+              "licensed under CC BY 4.0")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot"),
-           p("All the data displayed in this visualization is provided by the 
-             Italian Ministry of Health (Ministero della Salute) and elaborated 
-             by Dipartimento della Protezione Civile. This work is therefore a 
-             derivative of ",
-             a("COVID-19 Italia - Monitoraggio situazione",
-               href="https://github.com/pcm-dpc/COVID-19"),
-             "licensed under CC BY 4.0")
+           p("Code available ",
+             a("here",
+               href = "https://github.com/luismmontilla/covid-19-ita"),
+             align = "center")
         )
     )
 )
@@ -39,19 +51,41 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        df %>%
-            filter(denominazione_regione==input$regionInput) %>% 
-            ggplot(aes(x = data, y = nuovi_attualmente_positivi)) +
-            geom_point() +
-            geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")), 
-                       linetype="dashed", 
-                       color = "black") +
-            annotate("text", 
-                     x = as.POSIXct(as.Date("2020-03-07")), 
-                     y = max(df[df$denominazione_regione==input$regionInput,"nuovi_attualmente_positivi"]), 
-                     label = "Announcement of lockdown") +
-            labs(x = "Date",
-                 y = "New cases")
+        if(input$nationInput == TRUE){
+        
+            p <- region %>%
+                filter(denominazione_regione==input$regionInput) %>% 
+                ggplot(aes(x = data, y = nuovi_attualmente_positivi)) +
+                geom_point() +
+                geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")), 
+                           linetype="dashed", 
+                           color = "black") +
+                labs(x = "Date",
+                     y = "New cases")
+            
+            p + geom_line(aes(x = nation$data,
+                              y = nation$nuovi_attualmente_positivi)) +
+                annotate("text", 
+                         x = as.POSIXct(as.Date("2020-03-07")), 
+                         y = max(nation$nuovi_attualmente_positivi), 
+                         label = "Announcement of lockdown")
+        } else {
+            region %>%
+                filter(denominazione_regione==input$regionInput) %>% 
+                ggplot(aes(x = data, y = nuovi_attualmente_positivi)) +
+                geom_point() +
+                geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")), 
+                           linetype="dashed", 
+                           color = "black") +
+                annotate("text", 
+                         x = as.POSIXct(as.Date("2020-03-07")), 
+                         y = max(region[region$denominazione_regione==input$regionInput,"nuovi_attualmente_positivi"]), 
+                         label = "Announcement of lockdown") +
+                labs(x = "Date",
+                     y = "New cases")
+        }
+        
+        
     })
 }
 
