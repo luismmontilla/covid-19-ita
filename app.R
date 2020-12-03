f7311eeab3715c723982b731e2f4246bf3ac09a6
luismmontila <luismmontilla@gmail.com>
2020-12-03 19:01:19 +0100

    version 2

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(ggpubr)
 library(patchwork)
 
 #data
@@ -12,170 +13,273 @@ region$data <- as.POSIXct(sub(" .*", "", region$data))
 url_b <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
 nation <- read.csv(url_b)
 nation$data <- as.POSIXct(sub(" .*", "", nation$data))
+#
 
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    # Application title
-    titlePanel("COVID-19: Number of new cases in Italy"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("regionInput", 
-                        "Region",
-                        choices = unique(region$denominazione_regione)),
-            checkboxInput("nationInput", 
-                          label = "Show national data", 
-                          value = FALSE),
-            checkboxInput("trendInput", 
-                          label = "Show data trend", 
-                          value = FALSE),
-            p("All the data displayed in this visualization is provided by the 
+  
+  title = "COVID-19: Number of new cases in Italy",
+  
+  plotOutput('distPlot'),
+  
+  hr(),
+  
+  fluidRow(
+    column(3,
+           offset = 3,
+           h4("COVID-19 in Italy"),
+           selectInput("regionInput", 
+                       "Region",
+                       choices = c("(None)",unique(region$denominazione_regione))
+                       ),
+           p("Code available ",
+             a("here",
+               href = "https://github.com/luismmontilla/covid-19-ita"),
+             align = "center")
+    ),
+    column(3,
+           #offset = 2,
+           p("All the data displayed in this visualization is provided by the 
              Italian Ministry of Health (Ministero della Salute) and elaborated 
              by Dipartimento della Protezione Civile. This work is therefore a 
              derivative of ",
-              a("COVID-19 Italia - Monitoraggio situazione",
-                href = "https://github.com/pcm-dpc/COVID-19"),
-              "licensed under CC BY 4.0")
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot"),
-            p("A: Begining of the lockdown. B: Beginning of the Phase 2.",
-              align = "center"),
-            p("Code available ",
-              a("here",
-                href = "https://github.com/luismmontilla/covid-19-ita"),
-              align = "center")
-        )
+             a("COVID-19 Italia - Monitoraggio situazione",
+               href = "https://github.com/pcm-dpc/COVID-19"),
+             "licensed under CC BY 4.0")
+      
     )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+
+    #national plot
+    n1 <- nation %>%
+      filter(nuovi_positivi >= 0) %>%
+      ggplot() +
+      geom_point(aes(x = data, y = nuovi_positivi)) +
+      labs(x = "Date",
+           y = "New cases") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                 linetype="dashed",
+                 color = "black") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                 linetype = "dashed",
+                 color = "black") +
+      geom_smooth(aes(x = data, y = nuovi_positivi), method = "gam")
     
-    output$distPlot <- renderPlot({
-        
-        
-        #regional plot
-        p <- region %>%
-            filter(denominazione_regione==input$regionInput) %>% 
-            filter(nuovi_positivi >= 0) %>% 
-            ggplot(aes(x = data, y = nuovi_positivi)) +
-            geom_point() + 
-            geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
-                       linetype = "dashed",
-                       color = "black") +
-            geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
-                       linetype = "dashed",
-                       color = "black") +
-            labs(x = "Date",
-                 y = "New cases")
-        
-        #national plot
-        q <- nation %>% 
-            filter(nuovi_positivi >= 0) %>% 
-            ggplot(aes(x = data, y = nuovi_positivi)) +
-            geom_point() +
-            labs(x = "Date",
-                 y = "") +
-            geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")), 
-                       linetype="dashed", 
-                       color = "black") +
-            geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
-                       linetype = "dashed",
-                       color = "black")
-        
-        
-        if(input$nationInput == TRUE){
-            if(input$trendInput == TRUE) {
-                
-                p = p +
-                    ylim(0, max(nation$nuovi_positivi)) +
-                    geom_smooth() +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-03-07")),
-                             y = max(nation$nuovi_positivi),
-                             label = "A") +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-05-02")),
-                             y = max(nation$nuovi_positivi),
-                             label = "B")
-                
-                
-                q = q +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-03-07")),
-                             y = max(nation$nuovi_positivi),
-                             label = "A") +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-05-02")),
-                             y = max(nation$nuovi_positivi),
-                             label = "B") +
-                    geom_smooth()
-                
-                p+q
-                
-            } else {
-                
-                p = p +
-                    annotate("text",
-                                x = as.POSIXct(as.Date("2020-03-07")),
-                                y = max(nation$nuovi_positivi),
-                                label = "A") +
-                    ylim(0, max(nation$nuovi_positivi)) +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-05-02")),
-                             y = max(nation$nuovi_positivi),
-                             label = "B")
-                
-                q = q +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-03-07")),
-                             y = max(nation$nuovi_positivi),
-                             label = "A") +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-05-02")),
-                             y = max(nation$nuovi_positivi),
-                             label = "B")
-                
-                p+q
-            }
-            
-        } else {
-            if(input$trendInput == TRUE) {
-                p +
-                    geom_smooth() +
-                annotate("text",
-                         x = as.POSIXct(as.Date("2020-03-07")),
-                         y = max(region[region$denominazione_regione==input$regionInput,"nuovi_positivi"]),
-                         label = "A") +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-05-02")),
-                             y = max(region[region$denominazione_regione==input$regionInput,"nuovi_positivi"]),
-                             label = "B")
-                
-            } else {
-                p +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-03-07")),
-                             y = max(region[region$denominazione_regione==input$regionInput,"nuovi_positivi"]),
-                             label = "A") +
-                    annotate("text",
-                             x = as.POSIXct(as.Date("2020-05-02")),
-                             y = max(region[region$denominazione_regione==input$regionInput,"nuovi_positivi"]),
-                             label = "B")
-                
-            }
-            
-        }
-        
-        
-    })
-}
+    n2 <- nation %>%
+      #filter(nuovi_positivi >= 0) %>%
+      ggplot() +
+      geom_point(aes(x = data, y = totale_ospedalizzati)) +
+      labs(x = "Date",
+           y = "Hospitalized") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                 linetype="dashed",
+                 color = "black") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                 linetype = "dashed",
+                 color = "black") +
+      ylim(0, max(nation$nuovi_positivi))
+    
+    n3 <- nation %>%
+      #filter(nuovi_positivi >= 0) %>%
+      ggplot() +
+      geom_point(aes(x = data, y = deceduti)) +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                 linetype="dashed",
+                 color = "black") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                 linetype = "dashed",
+                 color = "black") +
+      labs(x = "Date",
+           y = "Deceased")
+    
+    n4 <- nation %>%
+      #filter(nuovi_positivi >= 0) %>%
+      ggplot() +
+      geom_point(aes(x = data, y = tamponi)) +
+      labs(x = "Date",
+           y = "PCR tests") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                 linetype="dashed",
+                 color = "black") +
+      geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                 linetype = "dashed",
+                 color = "black")
+    
+    
+    nt <- (n1 + n2 + n3 + n4) + 
+      plot_layout(ncol = 4)
+    
+    # tables for daily data
+    
+    nt1 <- nation %>% 
+      slice_max(data) %>% 
+      select(nuovi_positivi) %>% 
+      #data.frame("new_cases" = max(nation$nuovi_positivi)) %>% 
+      ggtexttable(rows = NULL, theme = ttheme(base_size = 18), 
+                  cols = "New positive cases")
+    
+    nt2 <- nation %>% 
+      slice_max(data) %>% 
+      select(totale_ospedalizzati) %>% 
+      ggtexttable(rows = NULL, cols = "Total hospitalized",
+                  theme = ttheme(base_size = 18))
+    
+    nt3 <- nation %>% 
+      slice_max(data) %>% 
+      select(deceduti) %>% 
+      ggtexttable(rows = NULL, cols = "Deceased",
+                  theme = ttheme(base_size = 18))
+    
+    nt4 <- nation %>% 
+      slice_max(data) %>% 
+      select(tamponi) %>% 
+      ggtexttable(rows = NULL, cols = "PCR tests",
+                  theme = ttheme(base_size = 18))
+    
+    ntt <- nt1+nt2+nt3+nt4 + plot_layout(ncol = 4)
+    
+    
+    nt/ntt
+    
+    if(input$regionInput != "(None)"){
+      
+      #regional plot
+      r1 <- region %>%
+        filter(denominazione_regione==input$regionInput) %>% 
+        #filter(denominazione_regione == "Campania") %>% 
+        filter(nuovi_positivi >= 0) %>% 
+        ggplot(aes(x = data, y = nuovi_positivi)) +
+        geom_point() + 
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                   linetype = "dashed",
+                   color = "black") +
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                   linetype = "dashed",
+                   color = "black") +
+        geom_smooth(aes(x = data, y = nuovi_positivi), method = "gam") +
+        labs(x = "Date",
+             y = "New cases")
+      
+      r2 <- region %>%
+        filter(denominazione_regione==input$regionInput) %>% 
+        #filter(denominazione_regione == "Campania") %>% 
+        filter(nuovi_positivi >= 0) %>% 
+        ggplot(aes(x = data, y = totale_ospedalizzati)) +
+        geom_point() + 
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                   linetype = "dashed",
+                   color = "black") +
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                   linetype = "dashed",
+                   color = "black") +
+        labs(x = "Date",
+             y = "Hospitalized") +
+        ylim(0, max(region[region$denominazione_regione==input$regionInput,"nuovi_positivi"]))
+      
+      r3 <- region %>%
+        filter(denominazione_regione==input$regionInput) %>% 
+        filter(nuovi_positivi >= 0) %>% 
+        ggplot(aes(x = data, y = deceduti)) +
+        geom_point() + 
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                   linetype = "dashed",
+                   color = "black") +
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                   linetype = "dashed",
+                   color = "black") +
+        labs(x = "Date",
+             y = "Deceased") 
+      
+      r4 <- region %>%
+        filter(denominazione_regione==input$regionInput) %>% 
+        filter(nuovi_positivi >= 0) %>% 
+        ggplot(aes(x = data, y = tamponi)) +
+        geom_point() + 
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-03-09")),
+                   linetype = "dashed",
+                   color = "black") +
+        geom_vline(xintercept = as.POSIXct(as.Date("2020-05-04")),
+                   linetype = "dashed",
+                   color = "black") +
+        labs(x = "Date",
+             y = "PCR tests") 
+      
+      rt <- r1 + r2 + r3 + r4 + 
+        plot_layout(ncol = 4) 
+      
+      # tables for daily data
+      
+      rt1 <- nation %>% 
+        slice_max(data) %>% 
+        mutate(area = "Italy") %>% 
+        select(area, nuovi_positivi) %>% 
+        bind_rows(region %>% 
+                    filter(denominazione_regione == input$regionInput) %>% 
+                    #filter(denominazione_regione == "Campania") %>% 
+                    filter(nuovi_positivi >= 0) %>%
+                    slice_max(data) %>%
+                    select(denominazione_regione, nuovi_positivi) %>% 
+                    rename(area = "denominazione_regione")
+                  ) %>% 
+        ggtexttable(rows = NULL, theme = ttheme(base_size = 18), 
+                    cols = c("Area","New positive cases"))
+      
+      rt2 <- nation %>% 
+        slice_max(data) %>% 
+        select(totale_ospedalizzati) %>% 
+        bind_rows(region %>% 
+                    filter(denominazione_regione == input$regionInput) %>% 
+                    #filter(denominazione_regione == "Campania") %>% 
+                    filter(nuovi_positivi >= 0) %>%
+                    slice_max(data) %>%
+                    select(totale_ospedalizzati)
+        ) %>% 
+        ggtexttable(rows = NULL, theme = ttheme(base_size = 18), 
+                    cols = c("Total hospitalized"))
+      
+      rt3 <- nation %>% 
+        slice_max(data) %>% 
+        select(deceduti) %>% 
+        bind_rows(region %>% 
+                    filter(denominazione_regione == input$regionInput) %>% 
+                    #filter(denominazione_regione == "Campania") %>% 
+                    filter(nuovi_positivi >= 0) %>%
+                    slice_max(data) %>%
+                    select(deceduti)
+        ) %>% 
+        ggtexttable(rows = NULL, theme = ttheme(base_size = 18), 
+                    cols = c("Deceased"))
+      
+      rt4 <- nation %>% 
+        slice_max(data) %>% 
+        select(tamponi) %>% 
+        bind_rows(region %>% 
+                    filter(denominazione_regione == input$regionInput) %>% 
+                    #filter(denominazione_regione == "Campania") %>% 
+                    filter(nuovi_positivi >= 0) %>%
+                    slice_max(data) %>%
+                    select(tamponi)
+        ) %>% 
+        ggtexttable(rows = NULL, theme = ttheme(base_size = 18), 
+                    cols = c("PCR tests"))
+      
+      rtt <- rt1+rt2+rt3+rt4+ plot_layout(ncol = 4) 
+      
+      nt/rt/rtt
+      
+
+
+    } else {
+      nt/ntt
+    }
+    
+
+  })
+ }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
